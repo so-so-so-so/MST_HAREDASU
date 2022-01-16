7d7eb0cd8eabd99835fd9635c1fd4b9e8290e1ce
so_ananta <crusader0710@gmail.com>
2022-01-17 02:06:35 +0900

    deploy

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,12 @@ Route::get('/home', 'HomeController@index')->name('home');
 //   Route::post('/regist_mail', 'UsersController@regist_mail')->name('regist_mail');
 // });
 
-Route::get('/vue', function () {
-  return view('app');
-});
-Route::get('/', function () {
-  return view('welcome');
-});
+// Route::get('/vue', function () {
+//   return view('app');
+// });
+// Route::get('/', function () {
+//   return view('welcome');
+// });
 
 Route::get('/{any}', function () {
   return view('welcome');
